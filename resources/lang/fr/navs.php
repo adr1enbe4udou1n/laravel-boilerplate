3a169cdabf4311ac4deae7d82634029f1d021a97
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2016-11-21 16:55:25 +0100

    lang

--- resources/lang/fr/navs.php
+++ resources/lang/fr/navs.php
@@ -2,19 +2,7 @@
 
 return [
     'backend' => [
-        'users' => [
-            'all' => 'Tous les utilisateurs',
-            'change-password' => 'Changer le mot de passe',
-            'create' => 'Créer un utilisateur',
-            'edit' => 'Éditer un utilisateur',
-            'main' => 'Utilisateurs'
-        ],
-
-        'sidebar' => [
-            'dashboard' => 'Tableau de bord',
-            'general' => 'Général',
-            'system' => 'System',
-        ],
+        'general' => 'Général',
     ],
 
     'frontend' => [
