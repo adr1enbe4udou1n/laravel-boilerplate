e686b98a824d0053af1e86fdfaa2326e9bd14656
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2016-11-29 21:44:05 +0100

    phpcs

--- app/Http/Middleware/MetaTags.php
+++ app/Http/Middleware/MetaTags.php
@@ -11,8 +11,9 @@ class MetaTags
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
