2c6fc30b79ba45cd50843de15ba3d23708e46c6c
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2016-12-01 21:35:28 +0100

    upgrade

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.3.25 on 2016-11-29.
+ * Generated for Laravel 5.3.26 on 2016-12-01.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
