a0594a7c9c023c9b7d2f90c02322a4f1b7fea7fd
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2016-12-10 17:17:18 +0100

    composer/yarn updade

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2016-12-04.
+    * Generated on 2016-12-10.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.3.26 on 2016-12-04.
+ * Generated for Laravel 5.3.26 on 2016-12-10.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
@@ -13942,4 +13942,6 @@ namespace {
 
 
 }
+
+
 
