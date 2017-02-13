0c4bb5e1815b6fefe44f1ab4bda9164c745e160b
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-02-13 20:03:08 +0100

    composer & yarn update

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.4.11 on 2017-02-10.
+ * Generated for Laravel 5.4.11 on 2017-02-13.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
