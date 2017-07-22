d43bb1b6c9565d73a866a5baab2c79384565e9f4
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-07-22 12:33:46 +0200

    upgrade

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2017-07-20.
+    * Generated on 2017-07-22.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
