aac7b63dfab2a25f6a3ea048f7d16c2398af96a2
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-07-24 20:31:38 +0200

    vue backend

--- app/Providers/FormServiceProvider.php
+++ app/Providers/FormServiceProvider.php
@@ -37,27 +37,8 @@ class FormServiceProvider extends ServiceProvider
                     }
                     break;
                 case 'components.form.select':
-                    if (!isset($parameters['type'])) {
-                        $parameters['type'] = 'select';
-                    }
-
                     if (isset($parameters['multiple']) && $parameters['multiple']) {
                         $attributes['multiple'] = true;
-                    }
-
-                    $attributes['data-toggle'] = $parameters['type'];
-
-                    if ($parameters['type'] === 'autocomplete') {
-                        $attributes = array_merge([
-                          'data-ajax-url' => $parameters['ajax_url'],
-                          'data-minimum-input-length' => $parameters['minimum_input_length'],
-                          'data-item-value' => $parameters['item_value'],
-                          'data-item-label' => $parameters['item_label'],
-                        ], $attributes);
-                    }
-
-                    if (isset($parameters['tags']) && $parameters['tags']) {
-                        $attributes['data-tags'] = 'true';
                     }
                     break;
                 case 'components.form.choices':
@@ -66,8 +47,8 @@ class FormServiceProvider extends ServiceProvider
                     }
 
                     $parameters['type'] = $parameters['multiple']
-                      ? 'checkboxes'
-                      : 'radios';
+                      ? 'checkbox'
+                      : 'radio';
                     break;
             }
 
@@ -96,15 +77,6 @@ class FormServiceProvider extends ServiceProvider
                 ], $attributes);
             }
 
-            if (isset($parameters['editor'])) {
-                $options = $parameters['editor'];
-
-                $attributes['data-toggle'] = 'editor';
-                if (isset($options['upload_url'])) {
-                    $attributes['data-upload-url'] = $options['upload_url'];
-                }
-            }
-
             // Merge attributes and view variables
             if (isset($parameters['attributes'])) {
                 $attributes = array_merge($attributes, $parameters['attributes']);
@@ -119,11 +91,8 @@ class FormServiceProvider extends ServiceProvider
         Form::component('bsTextarea', 'components.form.textarea', ['name', 'parameters' => []]);
         Form::component('bsSelect', 'components.form.select', ['name', 'parameters' => []]);
         Form::component('bsCheckbox', 'components.form.checkbox', ['name', 'parameters' => []]);
-        Form::component('bsToggle', 'components.form.toggle', ['name', 'parameters' => []]);
         Form::component('bsChoices', 'components.form.choices', ['name', 'parameters' => []]);
         Form::component('bsFile', 'components.form.file', ['name', 'parameters' => []]);
-        Form::component('bsImage', 'components.form.image', ['name', 'parameters' => []]);
-        Form::component('bsDatetime', 'components.form.datetime', ['name', 'parameters' => []]);
     }
 
     /**
--- resources/views/components/form/choices.blade.php
+++ resources/views/components/form/choices.blade.php
@@ -3,37 +3,44 @@
 @endif
 @foreach($choices as $key => $choice)
     @if(is_string($choice))
-        @php($value = $key)
-        @php($label = $choice)
+        @php
+            $value = $key;
+            $label = $choice;
+        @endphp
     @elseif(is_array($choice))
-        @php($value = isset($choice_value) ? $choice[$choice_value] : $key)
-        @php($label = isset($choice_label) ? $choice[$choice_label] : $key)
+        @php
+            $value = isset($choice_value) ? $choice[$choice_value] : $key;
+            $label = isset($choice_label) ? $choice[$choice_label] : $key;
+        @endphp
     @else
-        @php($value = isset($choice_value) ? $choice->$choice_value : $choice->id)
-        @php($label = isset($choice_label) ? $choice->$choice_label : $choice->__toString())
+        @php
+            $value = isset($choice_value) ? $choice->$choice_value : $choice->id;
+            $label = isset($choice_label) ? $choice->$choice_label : $choice->__toString();
+        @endphp
     @endif
     @if(isset($choice_tooltip))
         @if(is_string($choice))
-            @php($description = $choice)
+            @php
+                $description = $choice;
+            @endphp
         @elseif(is_array($choice))
-            @php($description = $choice[$choice_tooltip['title']])
+            @php
+                $description = $choice[$choice_tooltip['title']];
+            @endphp
         @else
-            @php($description = $choice->{$choice_tooltip['title']})
+            @php
+                $description = $choice->{$choice_tooltip['title']};
+            @endphp
         @endif
-        @php($label_attributes = [
-            'data-toggle' => 'tooltip',
-            'data-placement' => $choice_tooltip['position'],
-            'title' => trans($description)
-        ])
+        @php
+            $label_attributes = [
+                'data-toggle' => 'tooltip',
+                'data-placement' => $choice_tooltip['position'],
+                'title' => trans($description)
+            ];
+        @endphp
     @endif
-    @if($type === 'checkboxes')
-        @php($type = 'checkbox')
-    @endif
-    @if($type === 'radios')
-        @php($type = 'radio')
-    @endif
-    <label class="custom-control custom-{{ $type }}" {!! Html::attributes(isset($label_attributes) ? $label_attributes : []) !!}>
-
+    <label class="custom-control custom-{{ $type }}" {!! Html::attributes($label_attributes ?? []) !!}>
         {{ Form::$type($multiple ? "{$name}[]" : $name, $value, null, array_merge(['class' => 'custom-control-input'], $attributes)) }}
         <span class="custom-control-indicator"></span>
         <span class="custom-control-description">{{ trans($label) }}</span>
