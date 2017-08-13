a775154ec675bf8c494bd17e961730a70052fa11
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-08-13 14:27:29 +0200

    prod

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.4.32 on 2017-08-12.
+ * Generated for Laravel 5.4.32 on 2017-08-13.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
