f441290c235af284f634117f088b39ad0e9313bc
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-08-14 12:59:01 +0200

    sidebar

--- resources/assets/js/backend/components/Header.vue
+++ resources/assets/js/backend/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
     <header class="app-header navbar navbar-expand-lg">
-        <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">
+        <button class="navbar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">
             &#9776;
         </button>
         <a class="navbar-brand" href="#"></a>
         <ul class="nav navbar-nav d-md-down-none mr-auto">
             <li class="nav-item">
-                <a class="nav-link navbar-toggler sidebar-toggler" href="#" @click="sidebarMinimize">&#9776;</a>
+                <a class="nav-link" href="#" @click="sidebarMinimize">&#9776;</a>
             </li>
         </ul>
         <ul class="nav navbar-nav ml-auto mr-4">
