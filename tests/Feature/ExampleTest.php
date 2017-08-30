e0f01d88360738973fdae31ace462b3995ac67d5
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-08-30 20:58:22 +0200

    circleci

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      */
