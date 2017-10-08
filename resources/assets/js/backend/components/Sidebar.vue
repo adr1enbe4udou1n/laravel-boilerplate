21dcc0997b2f8cb4c2bdcee92bd73843be169901
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-10-08 16:01:16 +0200

    separate sidebar nav items from vue template

--- resources/assets/js/backend/components/Sidebar.vue
+++ resources/assets/js/backend/components/Sidebar.vue
@@ -2,66 +2,23 @@
     <div class="sidebar">
         <nav class="sidebar-nav">
             <ul class="nav">
-                <li class="nav-title">{{ $t('labels.general') }}</li>
-                <li class="nav-item">
-                    <router-link to="/dashboard" class="nav-link">
-                        <i class="icon-speedometer"></i> {{ $t('labels.backend.titles.dashboard') }}
-                    </router-link>
-                </li>
-
-                <template v-if="this.$app.blogEnabled && (this.$app.user.can('view own posts'))">
-                    <li class="nav-title">{{ $t('labels.backend.sidebar.content') }}</li>
-                    <li class="nav-item" v-if="this.$app.user.can('view own posts')">
-                        <router-link to="/posts" class="nav-link">
-                            <i class="icon-notebook"></i> {{ $t('labels.backend.posts.titles.main') }}
-                            <span class="badge badge-danger"
-                                  :title="$t('labels.backend.dashboard.new_posts')">{{ newPostsCount }}</span>
-                            <span class="badge badge-warning"
-                                  :title="$t('labels.backend.dashboard.pending_posts')">{{ pendingPostsCount }}</span>
-                        </router-link>
-                    </li>
-                </template>
-
-                <template v-if="this.$app.user.can('view form_submissions') || this.$app.user.can('view form_settings')">
-                    <li class="nav-title">{{ $t('labels.backend.sidebar.forms') }}</li>
-                    <li class="nav-item" v-if="this.$app.user.can('view form_submissions')">
-                        <router-link to="/form-submissions" class="nav-link">
-                            <i class="icon-list"></i> {{ $t('labels.backend.form_submissions.titles.main') }}
-                        </router-link>
-                    </li>
-                    <li class="nav-item" v-if="this.$app.user.can('view form_settings')">
-                        <router-link to="/form-settings" class="nav-link">
-                            <i class="icon-equalizer"></i> {{ $t('labels.backend.form_settings.titles.main') }}
-                        </router-link>
-                    </li>
-                </template>
-
-                <template v-if="this.$app.user.can('view users') || this.$app.user.can('view roles')">
-                    <li class="nav-title">{{ $t('labels.backend.sidebar.access') }}</li>
-                    <li class="nav-item" v-if="this.$app.user.can('view users')">
-                        <router-link to="/users" class="nav-link">
-                            <i class="icon-people"></i> {{ $t('labels.backend.users.titles.main') }}
-                        </router-link>
-                    </li>
-                    <li class="nav-item" v-if="this.$app.user.can('view roles')">
-                        <router-link to="/roles" class="nav-link">
-                            <i class="icon-shield"></i> {{ $t('labels.backend.roles.titles.main') }}
-                        </router-link>
-                    </li>
-                </template>
-
-                <template v-if="this.$app.user.can('view metas') || this.$app.user.can('view redirections')">
-                    <li class="nav-title">{{ $t('labels.backend.sidebar.seo') }}</li>
-                    <li class="nav-item" v-if="this.$app.user.can('view metas')">
-                        <router-link to="/metas" class="nav-link">
-                            <i class="icon-tag"></i> {{ $t('labels.backend.metas.titles.main') }}
-                        </router-link>
-                    </li>
-                    <li class="nav-item" v-if="this.$app.user.can('view redirections')">
-                        <router-link to="/redirections" class="nav-link">
-                            <i class="icon-control-forward"></i> {{ $t('labels.backend.redirections.titles.main') }}
-                        </router-link>
-                    </li>
+                <template v-for="item in navItems">
+                    <template v-if="item.access">
+                        <template v-if="item.title">
+                            <li class="nav-title">{{ item.name }}</li>
+                        </template>
+                        <template v-else>
+                            <li class="nav-item">
+                                <router-link :to="item.url" class="nav-link">
+                                    <i :class="item.icon"></i> {{ item.name }}
+                                    <template v-for="badge in item.badges">
+                                        <span :class="`badge badge-${badge.variant}`"
+                                              :title="badge.name">{{ badge.text }}</span>
+                                    </template>
+                                </router-link>
+                            </li>
+                        </template>
+                    </template>
                 </template>
             </ul>
         </nav>
@@ -69,34 +26,16 @@
     </div>
 </template>
 <script>
-  import axios from 'axios'
-
   export default {
     name: 'sidebar',
-    data () {
-      return {
-        newPostsCount: 0,
-        pendingPostsCount: 0
+    props: {
+      navItems: {
+        type: Array,
+        required: true,
+        default: () => []
       }
     },
-    created () {
-      this.fetchData()
-    },
     methods: {
-      fetchData () {
-        if (this.$app.user.can('manage own posts')) {
-          axios
-            .get(this.$app.route('admin.posts.draft.counter'))
-            .then(response => {
-              this.newPostsCount = response.data
-            })
-          axios
-            .get(this.$app.route('admin.posts.pending.counter'))
-            .then(response => {
-              this.pendingPostsCount = response.data
-            })
-        }
-      },
       handleClick (e) {
         e.preventDefault()
         e.target.parentElement.classList.toggle('open')
@@ -107,9 +46,6 @@
       brandMinimize () {
         document.body.classList.toggle('brand-minimized')
       }
-    },
-    watch: {
-      '$route': 'fetchData'
     }
   }
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -291,6 +291,7 @@ module.exports = {
     )
   ],
   resolve: {
+    extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
     }
