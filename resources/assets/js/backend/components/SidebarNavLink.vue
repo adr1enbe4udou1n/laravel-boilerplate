bd7866777df0ab41a68133b22ce99541a00452de
Vincent <vincent.blot14@gmail.com>
2017-10-09 11:24:33 +0200

    Update Sidebar with dropdown

---
+++ resources/assets/js/backend/components/SidebarNavLink.vue
@@ -0,0 +1,33 @@
+<template>
+    <router-link :to="url" class="nav-link">
+        <i :class="icon"></i> {{ name }}
+        <template v-for="badge in badges">
+            <span :class="`badge badge-${badge.variant}`"
+                  :title="badge.name">{{ badge.text }}</span>
+        </template>
+    </router-link>
+</template>
+
+<script>
+export default {
+  name: 'sidebar-nav-link',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    url: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    badges: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
