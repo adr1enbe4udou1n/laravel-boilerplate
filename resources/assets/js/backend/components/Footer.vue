9070410f5933354710269450349c6b616ac900d9
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-10-12 19:13:58 +0200

    update coreui

--- resources/assets/js/backend/components/Footer.vue
+++ resources/assets/js/backend/components/Footer.vue
@@ -3,18 +3,17 @@
         <strong>Copyright &copy; {{ new Date().getFullYear() }} <a
                 href="/" target="_blank">{{ name }}</a>.
         </strong> {{ $t("labels.all_rights_reserved") }}
-        <span class="float-right">
-
-        <template v-if="editorName !== ''">
-            <template v-if="editorSiteUrl !== ''">
-            <i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by <a
-                    :href="editorSiteUrl" target="_blank"><strong>{{ editorName }}</strong></a>
+        <span class="ml-auto">
+            <template v-if="editorName !== ''">
+                <template v-if="editorSiteUrl !== ''">
+                <i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by <a
+                        :href="editorSiteUrl" target="_blank"><strong>{{ editorName }}</strong></a>
+                </template>
+                <template v-else>
+                <i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by <strong>{{ editorName }}</strong>
+                </template>
             </template>
-            <template v-else>
-            <i class="fa fa-code"></i> with <i class="fa fa-heart"></i> by <strong>{{ editorName }}</strong>
-            </template>
-        </template>
-    </span>
+        </span>
     </footer>
 </template>
 <script>
