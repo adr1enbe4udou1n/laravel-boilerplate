ae1e22d8621325b3047e02bfadab78b99b557bb9
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2017-10-14 14:04:42 +0200

    fix search

--- resources/assets/js/backend/components/SidebarForm.vue
+++ resources/assets/js/backend/components/SidebarForm.vue
@@ -1,20 +1,15 @@
 <template>
   <div class="sidebar-form">
-    <b-form-input type="search" :placeholder="$t('labels.search')" @keyup="onSearch"
-                  v-model="query"></b-form-input>
+    <b-form-input type="search" :placeholder="$t('labels.search')" @input="onSearch"></b-form-input>
   </div>
 </template>
 <script>
   export default {
     name: 'sidebar-form',
-    data () {
-      return {
-        query: null
-      }
-    },
     methods: {
-      onSearch () {
-        this.$router.push({path: '/search', query: {q: this.query}})
+      onSearch (value) {
+        console.log('ok')
+        this.$router.push({path: '/search', query: {q: value}})
       }
     }
   }
