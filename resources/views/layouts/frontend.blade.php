f91e51223e35d443deaa0d0aed7d7e9527d0ec15
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-01-02 20:02:22 +0100

    test defer

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -67,8 +67,8 @@
     </script>
 
     <!-- CDN -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script defer src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 
     <!-- Scripts -->
     <script src="{{ Html::asset('manifest.js') }}"></script>
