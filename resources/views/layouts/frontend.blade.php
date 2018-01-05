62013b45c009f73552089cb3603f7f2478ff339f
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-01-05 20:43:46 +0100

    slim jq

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -67,7 +67,7 @@
     </script>
 
     <!-- CDN -->
-    <script defer src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script defer src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 
     <!-- Scripts -->
