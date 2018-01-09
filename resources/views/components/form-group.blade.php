18d55c77d466ae1e41702190089e46d8592f35e3
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-01-09 21:59:26 +0100

    refactor non-horizontal forms

--- resources/views/components/form-group.blade.php
+++ resources/views/components/form-group.blade.php
@@ -1,27 +1,32 @@
-<div class="form-group row">
+<div class="form-group @if(isset($horizontal) && $horizontal)row @endif">
     @isset($title)
-        <label for="{{ $name }}" class="{{ isset($horizontal) && $horizontal ? "col-md-{$label_cols} col-form-label" : 'col-12 form-control-label' }}">{{ $title }}</label>
+    <label for="{{ $name }}" @if(isset($horizontal) && $horizontal)class="col-md-{{ $label_cols }} col-form-label" @endif>{{ $title }}</label>
     @endisset
 
-    <div class="{{ isset($horizontal) && $horizontal ? 'col-md-' . (12 - $label_cols ) : 'col-12' }}">
+    @if(isset($horizontal) && $horizontal)
+    <div class="col-md-{{ (12 - $label_cols) }}">
+    @endif
 
         {{{ $slot }}}
 
         @if ($errors->has($name))
-            <div class="invalid-feedback">
-                <span v-if="errors.has('{{ $name }}')" v-cloak>@{{ errors.first('{{{ $name }}}') }}</span>
-                <span v-else>{{ $errors->first($name) }}</span>
-            </div>
+        <div class="invalid-feedback">
+            <span v-if="errors.has('{{ $name }}')" v-cloak>@{{ errors.first('{{{ $name }}}') }}</span>
+            <span v-else>{{ $errors->first($name) }}</span>
+        </div>
         @else
-            <div class="invalid-feedback" v-if="errors.has('{{ $name }}')">
-                @{{ errors.first('{{{ $name }}}') }}
-            </div>
+        <div class="invalid-feedback" v-if="errors.has('{{ $name }}')">
+            @{{ errors.first('{{{ $name }}}') }}
+        </div>
         @endif
 
         @if(isset($description))
-            <small class="form-text text-muted">
-                {{ $description }}
-            </small>
+        <small class="form-text text-muted">
+            {{ $description }}
+        </small>
         @endif
+
+    @if(isset($horizontal) && $horizontal)
     </div>
+    @endif
 </div>
