61729cd8d4faa8034d2bb63d194e2340974e5cf8
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-02-11 18:21:09 +0100

    test drone

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -4,9 +4,12 @@ namespace Tests\Browser;
 
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic browser test example.
      */
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      */
