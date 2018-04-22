10cc963711fce02e2d05b526f8190a92efcab2c6
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-04-22 16:50:51 +0200

    fix integer cast

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -69,6 +69,7 @@ class Post extends Model
      * @var array
      */
     protected $casts = [
+        'status' => 'integer',
         'pinned' => 'boolean',
         'promoted' => 'boolean',
     ];
