e78aa3f9aee32c23bf0d79faf5595723caf511ac
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-05-01 14:09:24 +0200

    fix about page url images

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -34,7 +34,7 @@
     <!-- CDN -->
     <script defer src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 
     <!-- Scripts -->
     <script defer src="{{ Html::asset('frontend', 'vendor-frontend.js') }}"></script>
