fec8f65b94d691a8365162bf1c27669b4c8ae5d1
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-06-30 12:25:10 +0200

    fix access navs

--- resources/assets/vendor/coreui/components/Sidebar/SidebarNav.vue
+++ resources/assets/vendor/coreui/components/Sidebar/SidebarNav.vue
@@ -3,40 +3,42 @@
     <VuePerfectScrollbar class="scroll-area" :settings="psSettings" @ps-scroll-y="scrollHandle">
       <ul class="nav">
         <template v-for="(item, index) in navItems">
-          <template v-if="item.title">
-            <SidebarNavTitle :key="index" :name="item.name" :classes="item.class" :wrapper="item.wrapper"></SidebarNavTitle>
-          </template>
-          <template v-else-if="item.divider">
-            <SidebarNavDivider :key="index" :classes="item.class"></SidebarNavDivider>
-          </template>
-          <template v-else-if="item.label">
-            <SidebarNavLabel :key="index" :name="item.name" :url="item.url" :icon="item.icon" :label="item.label" :classes="item.class"></SidebarNavLabel>
-          </template>
-          <template v-else>
-            <template v-if="item.children">
-              <!-- First level dropdown -->
-              <SidebarNavDropdown :key="index" :name="item.name" :url="item.url" :icon="item.icon">
-                <template v-for="(childL1, index1) in item.children">
-                  <template v-if="childL1.children">
-                    <!-- Second level dropdown -->
-                    <SidebarNavDropdown :key="index1" :name="childL1.name" :url="childL1.url" :icon="childL1.icon">
-                      <li :key="index2" class="nav-item" v-for="(childL2, index2) in childL1.children">
-                        <SidebarNavLink :name="childL2.name" :url="childL2.url" :icon="childL2.icon" :nav-items="navItems" :badge="childL2.badge" :variant="item.variant"></SidebarNavLink>
-                      </li>
-                    </SidebarNavDropdown>
-                  </template>
-                  <template v-else>
-                    <SidebarNavItem :key="index1" :classes="item.class">
-                      <SidebarNavLink :name="childL1.name" :url="childL1.url" :icon="childL1.icon" :nav-items="navItems" :badge="childL1.badge" :variant="item.variant"></SidebarNavLink>
-                    </SidebarNavItem>
-                  </template>
-                </template>
-              </SidebarNavDropdown>
+          <template v-if="item.access">
+            <template v-if="item.title">
+              <SidebarNavTitle :key="index" :name="item.name" :classes="item.class" :wrapper="item.wrapper"></SidebarNavTitle>
+            </template>
+            <template v-else-if="item.divider">
+              <SidebarNavDivider :key="index" :classes="item.class"></SidebarNavDivider>
+            </template>
+            <template v-else-if="item.label">
+              <SidebarNavLabel :key="index" :name="item.name" :url="item.url" :icon="item.icon" :label="item.label" :classes="item.class"></SidebarNavLabel>
             </template>
             <template v-else>
-              <SidebarNavItem :key="index" :classes="item.class">
-                <SidebarNavLink :name="item.name" :url="item.url" :icon="item.icon" :nav-items="navItems" :badge="item.badge" :variant="item.variant"></SidebarNavLink>
-              </SidebarNavItem>
+              <template v-if="item.children">
+                <!-- First level dropdown -->
+                <SidebarNavDropdown :key="index" :name="item.name" :url="item.url" :icon="item.icon">
+                  <template v-for="(childL1, index1) in item.children">
+                    <template v-if="childL1.children">
+                      <!-- Second level dropdown -->
+                      <SidebarNavDropdown :key="index1" :name="childL1.name" :url="childL1.url" :icon="childL1.icon">
+                        <li :key="index2" class="nav-item" v-for="(childL2, index2) in childL1.children">
+                          <SidebarNavLink :name="childL2.name" :url="childL2.url" :icon="childL2.icon" :nav-items="navItems" :badge="childL2.badge" :variant="item.variant"></SidebarNavLink>
+                        </li>
+                      </SidebarNavDropdown>
+                    </template>
+                    <template v-else>
+                      <SidebarNavItem :key="index1" :classes="item.class">
+                        <SidebarNavLink :name="childL1.name" :url="childL1.url" :icon="childL1.icon" :nav-items="navItems" :badge="childL1.badge" :variant="item.variant"></SidebarNavLink>
+                      </SidebarNavItem>
+                    </template>
+                  </template>
+                </SidebarNavDropdown>
+              </template>
+              <template v-else>
+                <SidebarNavItem :key="index" :classes="item.class">
+                  <SidebarNavLink :name="item.name" :url="item.url" :icon="item.icon" :nav-items="navItems" :badge="item.badge" :variant="item.variant"></SidebarNavLink>
+                </SidebarNavItem>
+              </template>
             </template>
           </template>
         </template>
